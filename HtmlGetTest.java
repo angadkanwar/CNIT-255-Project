12b72d07f6026d189a3bf8edeed8def28d577782
jpan2014 <pan117@purdue.edu>
2018-04-05 14:56:01 -0400

    Time-pulling API with comments
    Now has comments

--- HtmlGetTest.java
+++ HtmlGetTest.java
@@ -1,80 +1,31 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-// import org.json.JSONObject;
-// import org.json.JSONException;
-
-// public class HtmlGetTest {
-
-// 	public static String getHTML(String urlToRead) throws Exception {
-// 		StringBuilder result = new StringBuilder();
-// 		URL url = new URL(urlToRead);
-// 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-// 		conn.setRequestMethod("GET");
-// 		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-// 		String line;
-// 		while ((line = rd.readLine()) != null) {
-// 			result.append(line);
-// 		}
-// 		rd.close();
-// 		return result.toString();
-// 	}
-
-// 	public static void main(String[] args) throws Exception
-// 	{
-// 		System.out.println(getHTML(args[0]));
-// 	}
-// }
 
 public class HtmlGetTest {
 
-	public static String getHTML(String urlToRead) throws Exception {
-		StringBuilder result = new StringBuilder();
-		URL url = new URL(urlToRead);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-		rd.close();
-		return result.toString();
-	}
-
 	public static String getCurrentTime (String urlToRead) throws Exception {
 		StringBuilder result = new StringBuilder();
-		URL url = new URL(urlToRead);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		URL url = new URL(urlToRead); //creates link to URL passed in
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //opens connection to the URL
+		conn.setRequestMethod("GET"); //sets default request to get in order to pull information
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream())); //create continuous reader for stream
 		String line;
 		while ((line = rd.readLine()) != null) {
-			result.append(line);
-			//System.out.println(line);
+			result.append(line); //add to the line as stream continues
 		}
-		rd.close();
+		rd.close(); //close the reader when information is obtained
 		return result.toString();
-		// JSONObject jsonObj = new JSONObject(result);
-		// String time = jsonObj.getString("formatted");
-		// return time;
 	}
 
 	public static void main(String[] args) throws Exception
 	{
-		// System.out.println(getHTML(args[0]));
-		//System.out.println(getHTML("http://api.timezonedb.com/v2/get-time-zone?key=U5M24CM8VUSS&format=json&by=zone&zone=America/Indiana/Indianapolis"));
-		//String totalOutput = getHTML("http://api.timezonedb.com/v2/get-time-zone?key=U5M24CM8VUSS&format=json&by=zone&zone=America/Indiana/Indianapolis");
-		// String timeTotal = getCurrentTime("http://api.timezonedb.com/v2/get-time-zone?key=U5M24CM8VUSS&format=json&by=zone&zone=America/Indiana/Indianapolis");
-		// System.out.println(time.substring(285, time.length()-2));
-		System.out.println("Start here:");
 		String time = "";
 		while (true) {
+			// calls method to obtain full string of current time information
 			time = getCurrentTime("http://api.timezonedb.com/v2/get-time-zone?key=U5M24CM8VUSS&format=json&by=zone&zone=America/Indiana/Indianapolis");
-			System.out.println(time.substring(285, time.length()-2));
-			Thread.sleep(5000);
+			System.out.println(time.substring(285, time.length()-2)); //prints only the minimally necessary part
+			Thread.sleep(2000); //delays API call for 2 full seconds
 		}
-		
-		
 	}
 }
